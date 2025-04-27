693126fc814dda79b6caafab3710daa1e7363b7d
joaov-barbosa <95234827+joaov-barbosa@users.noreply.github.com>
2025-04-27 18:20:20 -0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 1.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.12.0"
+    }
+  }
+  cloud {
+    organization = "joaob"
+    workspaces {
+      name = "linuxtips-curso-containers-arquiteto-ecs-app"
+    }
+  }
+}
+
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+
 }
